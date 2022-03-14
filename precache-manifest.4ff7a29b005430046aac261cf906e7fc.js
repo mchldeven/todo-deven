473bc7198f547e5cf2addeadfc5b0704c34f328e
michaeldeven <michael.deven1999@gmail.com>
2022-03-14 21:51:34 +0700

    Updates

---
+++ precache-manifest.4ff7a29b005430046aac261cf906e7fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93826cce69f306cbba792acc08a0541",
+    "url": "/todo-deven/index.html"
+  },
+  {
+    "revision": "0455671711b7060272cc",
+    "url": "/todo-deven/static/css/main.45996b72.chunk.css"
+  },
+  {
+    "revision": "01391b49a4dda161fc84",
+    "url": "/todo-deven/static/js/2.fafc1f83.chunk.js"
+  },
+  {
+    "revision": "0455671711b7060272cc",
+    "url": "/todo-deven/static/js/main.1b75c196.chunk.js"
+  },
+  {
+    "revision": "3ce37df5b9905ae7671e",
+    "url": "/todo-deven/static/js/runtime~main.a7114913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-deven/static/media/logo.5d5d9eef.svg"
+  }
+]);
